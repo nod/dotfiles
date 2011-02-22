2fd6f75e46bd5cd9079e318de0a195840229f6b3
Jeremy Kelley <jeremy@collectivelabs.com>
2011-02-22 11:30:49 -0600

    moved some useful functions into a shared library, lib33.py. added ability for tmp33 to shorten urls via blink.to if an environment var is present

---
+++ bin/lib33.py
@@ -0,0 +1,35 @@
+import httplib
+import json
+import sys
+import pycurl
+from urllib import urlencode
+from json import loads as json_loads
+import StringIO
+
+
+def blinkto(uri):
+	headers = {'Content-type':'application/x-www-form-urlencoded', 'Accept':'text/plain'}
+	params = urlencode({'uri':uri})
+	conn = httplib.HTTPConnection('blink.to')
+	conn.request('PUT', '/', params, headers)
+	return json.loads(conn.getresponse().read()).get('uri')
+
+
+def upload_file(fpath, passwd):
+    out = StringIO.StringIO()
+    c = pycurl.Curl()
+    c.setopt(pycurl.WRITEFUNCTION, out.write)
+    c.setopt(c.POST, True)
+    c.setopt(c.URL, "https://33ad.org/tmp/up")
+    c.setopt(c.HTTPPOST, [
+        ("human", passwd),
+        ("submit", "tmp it."),
+        ("json", "1"),
+        ("upfile", (c.FORM_FILE, fpath)),
+        ] )
+    c.perform()
+    c.close()
+
+    ret = json_loads(out.getvalue())
+    return ret.get('uri')
+
