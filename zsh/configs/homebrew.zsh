4f37e2e434245e737ed7f0488e6a171b13d06d72
Jeremy Kelley <jeremy.kelley@exodusintel.com>
2021-04-22 13:34:12 -0500

    initial commit

---
+++ zsh/configs/homebrew.zsh
@@ -0,0 +1,6 @@
+# To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
+# Learn more about what you are opting in to at
+# https://docs.brew.sh/Analytics
+export HOMEBREW_NO_ANALYTICS=1
+export PATH=$PATH:/opt/homebrew/bin
+
